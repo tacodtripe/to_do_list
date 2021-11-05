e56165ce341629c8518d54bb5bd257419525e16b
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2021-11-05 14:39:32 -0700

    add missing elements

--- src/index.js
+++ src/index.js
@@ -7,32 +7,59 @@ import './style.css';
 const toDoListCointainer = document.getElementById('toDoListContainer');
 
 const toDoList = [
-  { description: 'demo 1', completed: true, index: 0 },
-  { description: 'demo 2', completed: false, index: 1 },
-  { description: 'demo 3', completed: false, index: 2 },
-  { description: 'demo 4', completed: true, index: 3 },
+  { description: 'demo 1 index 2', completed: true, index: 2 },
+  { description: 'demo 2 index 1', completed: false, index: 1 },
+  { description: 'demo 3 index 0', completed: false, index: 0 },
+  { description: 'demo 4 index 3', completed: true, index: 3 },
 ];
 
-function displayList(arr) {
-  arr.forEach((e) => {
+function orderToDisplay(arr) {
+  const orderedList = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    arr.forEach((e) => {
+      if (i === e.index) {
+        orderedList.push(e);
+      }
+    });
+  }
+  return orderedList;
+}
+
+function displayElement(arr) {
+  const orderedList = orderToDisplay(arr);
+  orderedList.forEach((e) => {
     const elementContainer = document.createElement('div');
     elementContainer.classList.add('row', 'justify-content-center', 'align-items-baseline');
     toDoListCointainer.appendChild(elementContainer);
 
+    const checkBoxContainer = document.createElement('div');
     const statusCheckBox = document.createElement('input');
+    checkBoxContainer.classList.add('col-1', 'justify-self-center', 'align-self-center');
     statusCheckBox.setAttribute('type', 'checkbox');
-    statusCheckBox.classList.add('col-1', 'justify-self-center', 'align-self-center');
-    elementContainer.appendChild(statusCheckBox);
+    statusCheckBox.classList.add('align-self-center');
+    checkBoxContainer.appendChild(statusCheckBox);
+    elementContainer.appendChild(checkBoxContainer);
 
     const description = document.createElement('p');
     description.innerText = e.description;
-    description.classList.add('col-4', 'justify-self-center', 'align-self-center');
+    description.classList.add('col-2', 'align-self-center');
     elementContainer.appendChild(description);
 
+    const iconContainer = document.createElement('div');
     const icon = document.createElement('i');
-    icon.classList.add('fas', 'fa-ellipsis-v', 'col-1', 'justify-self-center', 'align-self-center');
-    elementContainer.appendChild(icon);
+    iconContainer.classList.add('col-1', 'd-flex', 'justify-content-end');
+    icon.classList.add('fas', 'fa-ellipsis-v', 'mt-1');
+    iconContainer.appendChild(icon);
+    elementContainer.appendChild(iconContainer);
   });
+  const clearAllButtonContainer = document.createElement('div');
+  const clearAllButton = document.createElement('button');
+  clearAllButtonContainer.classList.add('row', 'justify-content-center');
+  clearAllButton.classList.add('col-4');
+  clearAllButton.id = 'clearAllButton';
+  clearAllButton.innerText = 'Clear all completed';
+  toDoListCointainer.appendChild(clearAllButtonContainer);
+  clearAllButtonContainer.appendChild(clearAllButton);
 }
 
-displayList(toDoList);
+displayElement(toDoList);
